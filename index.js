99336f984a8ac1dec001f14ff4185e6f1f123ab8
antonioomodho <antonioomodho@gmail.com>
2022-11-23 13:59:15 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+//function add() {
+    // we'll fill this in shortly
+  //}
+  //function subtract() {
+    // we'll fill this in shortly
+ // }
+  //function multiply() {
+    // we'll fill this in shortly
+  //}
+  //function divide() {
+    // we'll fill this in shortly
+  //}
+  function add(a, b) {
+    return a + b;
+  }
+//function add(a, b) {
+  //return 1078;
+//}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function  increment(n) {
+    n++
+    return n
+  }
+  function  decrement(n) {
+    n--
+    return n
+  }
+  function  makeInt(n) {
+    return n
+  }
+  function  preserveDecimal(n) {
+    return n
+  }
+ // parseInt("2", 10); //=> 2
+
+  function makeInt(n) {
+
+    (parseInt(n, 10))
+    return (parseInt(n, 10))
+  }
+  function preserveDecimal(n) {
+    (parseFloat(n))
+    return  (parseFloat(n))
+}
